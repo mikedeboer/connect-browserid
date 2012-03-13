9d90154e28275921404302a07d23743e722a012b
Austin King <shout@ozten.com>
2012-03-12 23:20:42 -0700

    Forgot to remove some console logs and example IP instead of hostname

--- example/run_example.js
+++ example/run_example.js
@@ -4,12 +4,11 @@ var app = express.createServer();
 
 var browserid = browserid_init({
     //audience: 'http://127.0.0.1:3030',
-    audience: 'http://haskwhal:3030',
+    audience: 'http://127.0.0.1:3030',
     debug: true,
     auth_next: '/',
     logout_next: '/'
 });
-console.log('1');
 app.set('views', __dirname + '/views');
 app.use(express.cookieParser());
 app.use(express.session({ secret: "keyboard cat" }));
@@ -19,17 +18,6 @@ app.use(function (req, resp, next) {
     resp.local('csrf', req.session._csrf);
     next();
 });
-console.log('2');
-app.use(browserid.authUser);
-console.log('after auth');
-app.post('/auth', browserid.auth);
-// Optional - use events to de-couple your app
-browserid.events.on('login', function (email, req, resp) {
-  console.log('User', email, 'logged in');
-});
-console.log('after login');
-app.post('/logout', browserid.logout);
-
 app.configure('development', function(){
     // A common error when developing locally is hitting your server as
     // http://127.0.0.1:3000 but setting up your audience as
@@ -37,6 +25,13 @@ app.configure('development', function(){
     app.use(browserid.guarantee_audience);
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
+app.use(browserid.authUser);
+app.post('/auth', browserid.auth);
+// Optional - use events to de-couple your app
+browserid.events.on('login', function (email, req, resp) {
+  console.log('User', email, 'logged in');
+});
+app.post('/logout', browserid.logout);
 
 app.get('/', function (req, resp) {
     if (req.email) {
